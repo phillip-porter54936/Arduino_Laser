cea5b2ac094c63b622de10dcdc90ebaacea973f6
phillip <phillip.porter234@myci.csuci.edu>
2017-11-30 10:43:19 -0800

    First commit

--- Arduino_Laser.ino
+++ Arduino_Laser.ino
@@ -4,6 +4,15 @@
 // COMP 469
 
 #include <Servo.h>
+
+#define START_BYTE '$'
+#define SEPARATOR  '/'
+#define END_BYTE   '!'
+#define ANGLE_BYTES 3
+#define DISTANCE_BYTES 3
+#define LENGTH 8
+
+
 void setup()
 {
   Serial.begin(9600);
@@ -11,14 +20,72 @@ void setup()
 
 void loop()
 {
-  static int count = 0;
-  if (Serial.available())
+  byte buffer[LENGTH];
+  char byte_read = '\0';
+  boolean found_start = false;
+  boolean found_end   = false;
+  
+  long angle    = 0;
+  long distance = 0;
+  
+  // Find the start
+  while(!found_start)
   {
-    Serial.print((char)(Serial.read()));
-    if (count++ > 60)
-    {
-      Serial.println("");
-      count = 0;
-    }
+      if (Serial.available() > 0)
+      {
+         byte_read = Serial.read();
+         found_start = (byte_read == START_BYTE); 
+      }
   }
+  
+  // Fill buffer
+  while (!found_end)
+  {
+    int index = 1;
+    if (Serial.available() > 0)
+    {
+      byte_read = Serial.read();
+      buffer[index++] = byte_read;
+      found_end = (byte_read == END_BYTE);
+    } 
+  }  
+  
+  // Get values
+  memcpy((byte*)&angle, buffer + 1, ANGLE_BYTES);
+  memcpy((byte*)&distance, buffer + 2 + ANGLE_BYTES, DISTANCE_BYTES);
+  
+  Serial.print("Angle: ");
+  Serial.print(angle);
+  Serial.print(", Distance: ");
+  Serial.println(distance);
+  
+  for (int i = 0; i < LENGTH; i++)
+  {
+      
+  }
+  
+  buffer[LENGTH-1] = '\0';
+  
+  Serial.println((char*)buffer);
+  
+  /* TODO: what to do with this
+  
+  // Parse reading
+  if (buffer.length() == LENGTH)
+  {
+    angle    = buffer.substring(0,ANGLE_BYTES).toInt();
+    distance = buffer.substring(ANGLE_BYTES+1,LENGTH-1).toInt();
+    
+    Serial.print("Angle: ");
+    Serial.print(angle);
+    Serial.print(", Distance: ");
+    Serial.println(distance);
+  }
+  else
+  {
+    Serial.print("\nERROR: length invalid: ");
+    Serial.print(buffer); 
+  }
+  */
 }
+
